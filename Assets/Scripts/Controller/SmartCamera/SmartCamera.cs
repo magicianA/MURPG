b78988add76fc0e8adf32fd5d0908f816fa8f4f4
Minstreams <minstreams@qq.com>
2020-03-10 16:39:12 +0800

    SmartCamera

---
+++ Assets/Scripts/Controller/SmartCamera/SmartCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// My Smart Camera，挂载到Camera上
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class SmartCamera : MonoBehaviour
+{
+    private Camera cam;
+
+    public class SmartCameraPoint
+    {
+        public Vector2 anchor;
+        public Vector2 offset;
+        public float distance;
+        public float fov;
+        public float weight;
+
+        public SmartCameraPoint(Vector2 anchor, Vector2 offset, float distance, float fov)
+        {
+            this.anchor = anchor;
+            this.offset = offset;
+            this.distance = distance;
+            this.fov = fov;
+            this.weight = 0;
+        }
+    }
+
+    public Transform target;
+    private Vector2 targetPos { get => target.position; }
+
+    public static List<SmartCameraPoint> cameraPoints = new List<SmartCameraPoint>();
+
+    //如果把Camera改写成系统，这几个配置应该放到类似CameraSystemSetting的配置中去
+    public float reactDistance = 0.1f;
+    [Range(0.01f, 0.95f)]
+    public float movingRate = 0.1f;
+
+    /// <summary>
+    /// 后坐力效果
+    /// </summary>
+    public void React(Vector3 direction)
+    {
+        transform.Translate(direction * reactDistance);
+    }
+
+
+    private void Awake()
+    {
+        cameraPoints.Clear();
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        float fullWeight = 0;
+        for (int i = 0; i < cameraPoints.Count; i++)
+        {
+            float d = Vector2.Distance(targetPos, cameraPoints[i].anchor);
+            float weight = 1.0f / (d + 0.001f);
+            cameraPoints[i].weight = weight;
+            fullWeight += weight;
+        }
+        Vector2 target2D = targetPos;
+        float targetZ = 0;
+        float targetFov = 0;
+        for (int i = 0; i < cameraPoints.Count; i++)
+        {
+            float factor = cameraPoints[i].weight / fullWeight;
+            target2D += cameraPoints[i].offset * factor;
+            targetZ += cameraPoints[i].distance * factor;
+            targetFov += cameraPoints[i].fov * factor;
+        }
+        Vector3 target3D = new Vector3(target2D.x, target2D.y, targetZ);
+
+        float t = 1.0f - Mathf.Pow(1.0f - movingRate, Time.deltaTime / Time.timeScale);
+        transform.position = Vector3.Lerp(transform.position, target3D, t);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFov, t);
+    }
+}
---
+++ Assets/Scripts/Controller/SmartCamera/SmartCameraRecorder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 摄像机机位，应该放在场景中
+/// TODO：可以给SmartCameraPoint加上weight属性
+/// </summary>
+public class SmartCameraRecorder : MonoBehaviour
+{
+    public Vector2 offset;
+    public float fov = 60;
+
+    private void Start()
+    {
+        SmartCamera.cameraPoints.Add(new SmartCamera.SmartCameraPoint((Vector2)transform.position + offset, -offset, transform.position.z, fov));
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!Camera.current) return;
+        Gizmos.color = Color.gray;
+        float theta = fov * Mathf.PI / 360f;
+        float height = -transform.position.z * Mathf.Tan(theta);
+        float width = height * Camera.current.aspect;
+
+        Gizmos.DrawWireSphere((Vector3)((Vector2)transform.position + offset), 0.25f);
+
+        Gizmos.DrawLine(new Vector3(transform.position.x - width, transform.position.y + height), new Vector3(transform.position.x + width, transform.position.y + height));
+        Gizmos.DrawLine(new Vector3(transform.position.x - width, transform.position.y + height), new Vector3(transform.position.x - width, transform.position.y - height));
+        Gizmos.DrawLine(new Vector3(transform.position.x + width, transform.position.y - height), new Vector3(transform.position.x + width, transform.position.y + height));
+        Gizmos.DrawLine(new Vector3(transform.position.x + width, transform.position.y - height), new Vector3(transform.position.x - width, transform.position.y - height));
+
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + width, transform.position.y + height));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + width, transform.position.y - height));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x - width, transform.position.y - height));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x - width, transform.position.y + height));
+
+    }
+}
