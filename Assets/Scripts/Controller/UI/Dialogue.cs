b4c9e69bc9a44f7fb661277f1d034766978965bc
ryphy <ryphy0420@gmail.com>
2020-04-05 15:56:04 +0800

    对话

---
+++ Assets/Scripts/Controller/UI/Dialogue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextAsset file;
+    public Dictionary<string, Sprite> nameDictionary;
+    List<string> stringList = new List<string>();
+    private void Start()
+    {
+        string[] fileData = file.ToString().Split('\n');
+        stringList.Clear();
+        for (int i = 0; i < fileData.Length; i++)
+        {
+            stringList.Add(fileData[i]);
+
+        }
+        transform.Find("Name").GetComponent<Text>().text = stringList[0];
+        transform.Find("Avatar").GetComponent<Image>().sprite = nameDictionary[stringList[0].Trim()];
+        stringList.RemoveAt(0);
+        transform.Find("Content").GetComponent<Text>().text = stringList[0];
+        stringList.RemoveAt(0);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (stringList.Count > 0)
+            {
+                transform.Find("Name").GetComponent<Text>().text = stringList[0];
+                transform.Find("Avatar").GetComponent<Image>().sprite = nameDictionary[stringList[0].Trim()];
+                stringList.RemoveAt(0);
+                transform.Find("Content").GetComponent<Text>().text = stringList[0];
+                stringList.RemoveAt(0);
+            }
+            else
+                Destroy(gameObject);
+        }
+        //if (Input.GetMouseButtonDown(0)&&stringList.Count == 0)
+        //    Destroy(gameObject);
+    }
+}
