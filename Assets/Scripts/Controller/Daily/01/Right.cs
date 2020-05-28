72f6ae69a787792e26c2bad33d7ed744c369dadd
ryphy <ryphy0420@gmail.com>
2020-05-28 19:43:56 +0800

    音乐修改

--- Assets/Scripts/Controller/Daily/01/Right.cs
+++ Assets/Scripts/Controller/Daily/01/Right.cs
@@ -8,47 +8,27 @@ public class Right : MonoBehaviour
     Backpack backpack;
     public TextAsset text;
     private bool check = false;
-    private float timer = 0f;
+    private int count = 0;
     private List<string> checkList = new List<string>();
     private void Start()
     {
         backpack = GameObject.FindWithTag("Backpack").GetComponent<Backpack>();
         knife = new InteractiveItem("Daily/道具/刀","刀");
         checkList.Clear();
+        count = 0;
     }
     private void Update()
     {
         if (check&&!backpack.ContainsItem("刀"))
         {
-            timer += Time.deltaTime ;
             if(MyInput.isButtonDown)
             {
-                if (timer > 2.25f && timer < 3.75f)
-                {
-                    checkList.Add("长");
-                }
-
-                else if (timer > 0.5f && timer < 2f)
-                {
-                    checkList.Add("短");
-                }
-                else
-                {
-                    checkList.Clear();
-                }
-                timer = 0;
-                
+                count++;
             }
-            if (checkList.Count == 4)
+            if (count == 4)
             {
-                if (checkList[0] == "长" && checkList[1] == "短"  &&
-                    checkList[2] == "长" && checkList[3] == "短" )
-                {
-                    backpack.AddItem(knife);
-                    check = false;
-                }
-                else
-                    checkList.Clear();
+                backpack.AddItem(knife);
+                check = false;
             }
         }
     }
